ea90b3e9127eca105c26bd14e4e083956b9415f0
Winston Haaswijk <winston.haaswijk@gmail.com>
2013-07-04 19:05:28 +0200

    Porting sqlite plugin to WP8.

---
+++ Plugins/Cirrious/Sqlite/Cirrious.MvvmCross.Plugins.Sqlite.WindowsPhone/Bootstrap/SqlitePluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace Cirrious.MvvmCross.Plugins.Sqlite.WindowsPhone.Bootstrap
+{
+    public class SqlitePluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Sqlite.PluginLoader>
+    {
+    }
+}
